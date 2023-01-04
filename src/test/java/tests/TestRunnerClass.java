87605df173799dbb32bea21a3bbd849c8171f371
Manigandan1797 <Manigandan.r1797@gmail.com>
2023-01-04 20:49:18 +0530

    Commit

---
+++ src/test/java/tests/TestRunnerClass.java
@@ -0,0 +1,16 @@
+package tests;
+
+import org.junit.runner.RunWith;
+
+import com.intuit.karate.KarateOptions;
+import com.intuit.karate.junit4.Karate;
+
+@RunWith(Karate.class)
+@KarateOptions(tags= {"@GTQTOGBP"})
+public class TestRunnerClass {
+	
+ 
+	 
+ 
+
+}
